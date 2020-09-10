93439a24f5de41103e68642c562daab48ea851e6
Radek <rylkoradoslaw230@gmail.com>
2020-09-10 23:30:39 +0200

    Initialize class method added

---
+++ Window.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+class Window():
+    def __init__(self, window_width, window_height, fullscreen):
+        pygame.init()
+        self.window_width = window_width
+        self.window_height = window_height
+        self.fullscreen = fullscreen
+
+
+
+
+
+        
