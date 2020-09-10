4ac4ae637a11d17355b01b00dae8d944c0d13995
Radek <rylkoradoslaw230@gmail.com>
2020-09-10 23:47:13 +0200

    Added window init method

--- Window.py
+++ Window.py
@@ -7,7 +7,13 @@ class Window():
         self.window_width = window_width
         self.window_height = window_height
         self.fullscreen = fullscreen
+        self.window_init()
 
+    def window_init(self):
+        pygame.display.set_mode((self.window_width, self.window_height), display=self.fullscreen)
+
+
+app = Window(400, 400, 0)
 
 
 
