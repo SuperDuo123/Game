fbb04a37e81979faaa174234c609f3c5f7a5310c
Radek <rylkoradoslaw230@gmail.com>
2020-09-11 23:05:09 +0200

    Added Menu class

---
+++ Menu.py
@@ -0,0 +1,14 @@
+import pygame
+from settings import *
+from pygame import surface, display, rect
+
+class Menu():
+
+    def __init__(self):
+        background_surface = pygame.surface.Surface((window_width, window_height))
+        buttons_surface = pygame.surface.Surface((window_width, window_height))
+
+
+menu = Menu()
+
+
