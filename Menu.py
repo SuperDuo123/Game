d9473485fc25fc3364d873a02f42fdf11b01dd78
Radek <rylkoradoslaw230@gmail.com>
2020-09-11 23:31:24 +0200

    Added load_images() method

--- Menu.py
+++ Menu.py
@@ -11,6 +11,16 @@ class Menu():
     def check_surfaces(self):
         print(self.background_surface)
         print(self.buttons_surface)
+
+
+    def load_images(self):
+        #do zmiany. Sprobuj uzyc generatorow yield etc.
+        self.menu_image = pygame.image.load("Menu/temp_menu.png").convert_alpha()
+        self.sound_image = pygame.image.load("Menu/dzwiek.png").convert_alpha()
+        self.fullscreen_image = pygame.image.load("Menu/full.png").convert_alpha()
+        self.start_image = pygame.image.load("Menu/start.png").convert_alpha()
+        self.exit_image = pygame.image.load("Menu/exit.png").convert_alpha()
+
 #menu = Menu()
 
 
