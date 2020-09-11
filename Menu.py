468ae05fa2e5b37cc160f39d84a7877b4040ba03
Radek <rylkoradoslaw230@gmail.com>
2020-09-11 23:13:05 +0200

    Check surfaces method

--- Menu.py
+++ Menu.py
@@ -5,10 +5,12 @@ from pygame import surface, display, rect
 class Menu():
 
     def __init__(self):
-        background_surface = pygame.surface.Surface((window_width, window_height))
-        buttons_surface = pygame.surface.Surface((window_width, window_height))
+        self.background_surface = pygame.surface.Surface((window_width, window_height))
+        self.buttons_surface = pygame.surface.Surface((window_width, window_height))
 
-
-menu = Menu()
+    def check_surfaces(self):
+        print(self.background_surface)
+        print(self.buttons_surface)
+#menu = Menu()
 
 
--- Window.py
+++ Window.py
@@ -1,14 +1,20 @@
 import pygame
+from Menu import Menu
 
 
 class Window():
     def __init__(self, window_width, window_height, fullscreen):
         pygame.init()
+        self.menu = Menu()
+
+
         self.window_width = window_width
         self.window_height = window_height
         self.fullscreen = fullscreen
         self.window_init()
         self.mainloop()
+        
+        
 
     def window_init(self):
         #FULLSCREEN maximizes the window without fitting the resolution
@@ -21,6 +27,7 @@ class Window():
     def mainloop(self):
         run = True
         while run:
+            self.menu.check_surfaces()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
