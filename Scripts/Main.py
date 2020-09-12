a4491e69f0bec389c349c3e999117c830a8d2f07
Radek <rylkoradoslaw230@gmail.com>
2020-09-12 15:39:25 +0200

    Scripts folder added

---
+++ Scripts/Main.py
@@ -0,0 +1,13 @@
+from Window import Window
+from settings import *
+# launch program from this file
+
+def main():
+    app = Window(window_width,window_height,0)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
