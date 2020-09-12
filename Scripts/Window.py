e816683ace971657b07f94bf5d80a85387745db7
Radek <rylkoradoslaw230@gmail.com>
2020-09-13 01:22:40 +0200

    changed to self.run, tossed self to Menu class

--- Scripts/Window.py
+++ Scripts/Window.py
@@ -1,12 +1,13 @@
 import pygame
-from Menu import Menu
+from Menu import *
 
 
 class Window():
     def __init__(self, window_width, window_height, fullscreen):
         #initialize pygame, menu classes
         pygame.init()
-        self.menu = Menu()
+
+        self.menu = Menu(self) #tosses window class attributes and functions to Menu class
 
         #window initialization parameters
         self.window_width = window_width
@@ -38,8 +39,8 @@ class Window():
 
     def mainloop(self):
 
-        run = True
-        while run:
+        self.run = True
+        while self.run:
             #menu background blitting
             self.screen.blit(self.menu.background_surface, (0, 0))
             self.screen.blit(self.menu.buttons_surface, (0, 0))
@@ -51,6 +52,7 @@ class Window():
                     """Left Mouse Button is associated with id. 1 of event.button. It's in-built function of pygame."""
                     if event.button == 1: #Left Mouse Button Click
                         self.menu.button_event_listener(pygame.mouse.get_pos())
+                        #self.menu.button_handler()
                         print(pygame.mouse.get_pos())
 
                     if event.button == 2: #Middle Mouse Button Click
